0e2e533b4d7399a6cf42f837df20f9ccce3b7002
Nick <94140534+NickBon7@users.noreply.github.com>
2022-03-19 22:48:24 +0000

    table with search

---
+++ FilteringTable/src/App.js
@@ -0,0 +1,13 @@
+//npm install react-table
+
+import FilteringTable from "./components/FilteringTable";
+
+function App() {
+  return (
+    <div>
+      <FilteringTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ FilteringTable/src/components/FilteringTable.js
@@ -0,0 +1,65 @@
+import React, { useMemo } from "react";
+import { useTable, useGlobalFilter } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./Columns";
+import "./BasicTable.css";
+import GlobalFilter from "./GlobalFilter";
+
+const BasicTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter
+  );
+
+  const { globalFilter } = state;
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>
+                  {" "}
+                  {column.render("Header")}{" "}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default BasicTable;
