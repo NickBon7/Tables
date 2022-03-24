a846d785653cf528b773ce51b0d89476a4f22b61
Nick <94140534+NickBon7@users.noreply.github.com>
2022-03-24 22:44:59 +0000

    table with column hiding

---
+++ ColumnHiding/src/App.js
@@ -0,0 +1,13 @@
+//npm install react-table
+
+import ColumnHiding from "./components/ColumnHiding";
+
+function App() {
+  return (
+    <div>
+      <ColumnHiding />
+    </div>
+  );
+}
+
+export default App;
---
+++ ColumnHiding/src/components/ColumnHiding.js
@@ -0,0 +1,65 @@
+import React, { useMemo } from "react";
+import { useTable } from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json";
+import { COLUMNS } from "./Columns";
+import { Checkbox } from "reakit/Checkbox";
+import "./BasicTable.css";
+
+const ColumnHiding = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    allColumns,
+  } = useTable({
+    columns,
+    data,
+  });
+
+  return (
+    <>
+      <div>
+        {allColumns.map((column) => (
+          <div key={column.id} className="toggleMenu">
+            <label>
+              <input type="checkbox" {...column.getToggleHiddenProps()} />
+              {column.Header}
+            </label>
+          </div>
+        ))}
+      </div>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")}</td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ColumnHiding;
